658cb8c5f3e1cc15eca96134ffa3c062ae40384a
chatinun <chatinun.hobby@gmail.com>
2023-10-27 16:48:18 +0700

    React Router Setup

---
+++ src/pages/Clockin-out/ClockinMainPage.jsx
@@ -0,0 +1,5 @@
+export default function ClockinMainPage() {
+  return (
+    <div>ClockinMainPage</div>
+  )
+}
---
+++ src/pages/ManagePages/IncomingRequest/IncomingRequestMainPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function IncomingRequestMainPage() {
+  return (
+    <div>IncomingRequestMainPage</div>
+  )
+}
