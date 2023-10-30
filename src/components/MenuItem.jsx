13f0e437b70b45372627fbd54ff0a73b038a5cc5
TeepakornSri <teepakorn.sriaree@gmail.com>
2023-10-30 14:52:54 +0700

    clockinPage

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export default function MenuItem() {
     return (
         <div className="flex w-full justify-evenly p-2 items-center">
-            <Link to='/profile'>
+            <Link to='/profile/record'>
                 <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
                     <div className="text-2xl text-white hover:text-green-400"><BsPersonFill /></div>
                     <h1 className="text-sm">Profile</h1>
