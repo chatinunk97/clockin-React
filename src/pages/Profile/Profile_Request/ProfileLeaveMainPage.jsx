a012181563fdec99a32efe29c261007c3c040e07
Prapawin <maiprapawin@gmail.com>
2023-10-30 17:53:37 +0700

    updated profile leave main page

--- src/pages/Profile/Profile_Request/ProfileLeaveMainPage.jsx
+++ src/pages/Profile/Profile_Request/ProfileLeaveMainPage.jsx
@@ -1,5 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function ProfileLeaveMainPage() {
-  return <Outlet />;
+  return (
+    <div>
+      <div className="flex justify-evenly items-center p-6 text-2xl font-semibold text-green-600 md:justify-center md:gap-20 ">
+        <Link to="/profile/leave">
+          <div>
+            <button className="hover:text-green-400 shadow-xl rounded-full p-4 w-40">
+              Leave Form
+            </button>
+          </div>
+        </Link>
+        <Link to="/profile/myleave">
+          <div>
+            <button className="hover:text-green-400 shadow-xl rounded-full p-4 w-40">
+              My Leave
+            </button>
+          </div>
+        </Link>
+      </div>
+      <Outlet />
+    </div>
+  );
 }
