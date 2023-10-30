560802da402200810e44506b5510e118e9f2c595
Prapawin <maiprapawin@gmail.com>
2023-10-30 18:52:45 +0700

    updated UI LeaveFormPage

---
+++ src/components/Profile/LeaveInfo.jsx
@@ -0,0 +1,36 @@
+import { AiFillCalendar } from "react-icons/ai";
+import { BsFillClipboard2Fill } from "react-icons/bs";
+import Dropdown from "../Dropdown";
+export default function LeaveInfo() {
+  return (
+    <div>
+      <div className="flex items-center gap-6 w-[300px]">
+        <button className="text-3xl text-slate-700">
+          <AiFillCalendar />
+        </button>
+        <div className="text-lg border-b border-b-neutral-400 w-[300px] text-center cursor-none">
+          Start Date
+        </div>
+      </div>
+      <div className="flex items-center gap-6 w-[300px]">
+        <button className="text-3xl text-slate-700">
+          <AiFillCalendar />
+        </button>
+        <div className="text-lg border-b border-b-neutral-400 w-[300px] text-center cursor-none">
+          End Date
+        </div>
+      </div>
+      <Dropdown type="day" />
+      <div className="flex items-center">
+        <div className="text-3xl text-slate-700">
+          <BsFillClipboard2Fill />
+        </div>
+        <input
+          type="text"
+          placeholder="Leave Information"
+          className="border-b border-b-neutral-400  text-start p-4 focus:ring focus:ring-green-300  focus:border-green-500 outline-none"
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/Profile/Profile_Request/LeaveFormPage.jsx
+++ src/pages/Profile/Profile_Request/LeaveFormPage.jsx
@@ -1,20 +1,23 @@
+import Dropdown from "../../../components/Dropdown";
+import LeaveInfo from "../../../components/Profile/LeaveInfo";
 import LeaveList from "../../../components/Profile/LeaveList";
 import SubmitButton from "../../../components/SubmitButton";
 
 export default function LeaveFormPage() {
   return (
     <div>
-      <div>
-        <div className="bg-inputGray m-6 p-6 rounded-md">
-          <LeaveList leaveName="Sick Leave" leaveAmount="15 days" />
-          <LeaveList leaveName="Annual Leave" leaveAmount="30 days" />
-          <LeaveList leaveName="Business Leave" leaveAmount="3 days" />
-        </div>
+      <div className="bg-inputGray m-6 p-6 rounded-md">
+        <LeaveList leaveName="Sick Leave" leaveAmount="15 days" />
+        <LeaveList leaveName="Annual Leave" leaveAmount="30 days" />
+        <LeaveList leaveName="Business Leave" leaveAmount="3 days" />
+      </div>
+      <div className="flex flex-col justify-center items-center">
         <div>
-          <div>dropdown</div>
-          <div>calendar</div>
+          <Dropdown />
+          <h2 className="text-lg font-bold"></h2>
+          <LeaveInfo />
         </div>
-        <div className="text-center">
+        <div className="mt-10">
           <SubmitButton p="px-20 py-3">Submit</SubmitButton>
         </div>
       </div>
