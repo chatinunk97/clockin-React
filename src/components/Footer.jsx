6ade487b0b1b54ac773c3cf85543fc763d9db5c4
Prapawin <maiprapawin@gmail.com>
2023-10-31 16:04:39 +0700

    refactored UI footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,35 @@
+import { useLocation } from "react-router-dom";
+import {
+  BsPersonFill,
+  BsFillAlarmFill,
+  BsFillPeopleFill,
+  BsCalendarEvent,
+  BsClockFill,
+} from "react-icons/bs";
 import MenuItem from "./MenuItem";
 
+const menus = [
+  { id: 1, to: "/profile", icon: <BsPersonFill />, name: "Profile" },
+  { id: 2, to: "/clockin", icon: <BsFillAlarmFill />, name: "Clock In" },
+  { id: 3, to: "/profile/people", icon: <BsFillPeopleFill />, name: "People" },
+  { id: 4, to: "/profile/leave", icon: <BsCalendarEvent />, name: "Leave" },
+  { id: 5, to: "/profile/ot", icon: <BsClockFill />, name: "OT" },
+];
+
 export default function Footer() {
+  const { pathname } = useLocation();
+
   return (
-    <div className="fixed bottom-0 flex flex-col justify-evenly items-center w-full py-5 bg-blue-100 ">
-      <MenuItem />
+    <div className="fixed bottom-0 flex justify-evenly items-center w-full py-5 bg-blue-100 ">
+      {menus.map((el) => (
+        <MenuItem
+          key={el.id}
+          to={el.to}
+          icon={el.icon}
+          name={el.name}
+          active={pathname === el.to}
+        />
+      ))}
     </div>
   );
 }
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,55 +1,64 @@
-import {
-    BsPersonFill,
-    BsFillAlarmFill,
-    BsFillPeopleFill,
-    BsCalendarEvent,
-    BsClockFill,
-} from "react-icons/bs";
 import { Link } from "react-router-dom";
 
-export default function MenuItem() {
-    return (
-        <div className="flex w-full justify-evenly p-2 items-center">
-            <Link to="/profile">
-                <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
-                    <div className="text-2xl text-white hover:text-green-400">
-                        <BsPersonFill />
-                    </div>
-                    <h1 className="text-sm">Profile</h1>
-                </div>
-            </Link>
-            <Link to="/clockin">
-                <div className="flex flex-col justify-center items-center  rounded-full p-2 w-18 h-18 gap-1 bg-slate-300  hover:text-green-700">
-                    <div className="text-2xl text-white hover:text-green-400">
-                        <BsFillAlarmFill />
-                    </div>
-                    <h1 className="text-sm">Clock In</h1>
-                </div>
-            </Link>
-            <Link to="/profile/people">
-                <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
-                    <div className="text-2xl text-white hover:text-green-400">
-                        <BsFillPeopleFill />
-                    </div>
-                    <h1 className="text-sm">People</h1>
-                </div>
-            </Link>
-            <Link to="/profile/leave">
-                <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
-                    <div className="text-2xl text-white hover:text-green-400">
-                        <BsCalendarEvent />
-                    </div>
-                    <h1 className="text-sm">Leave</h1>
-                </div>
-            </Link>
-            <Link to="/profile/OT">
-                <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
-                    <div className="text-2xl text-white hover:text-green-400">
-                        <BsClockFill />
-                    </div>
-                    <h1 className="text-sm">OT</h1>
-                </div>
-            </Link>
-        </div>
-    );
+export default function MenuItem({ to, icon, name, active }) {
+  return (
+    <div className="flex w-full justify-evenly p-2 items-center">
+      <div className={`${active ? "text-primaryGreen" : "text-gray-500"}`}>
+        <Link to={to}>
+          <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
+            <div className="text-2xl text-white hover:text-green-400 ">
+              {icon}
+            </div>
+            <h1 className="text-sm">{name}</h1>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+
+  // return (
+
+  //     <div className="flex w-full justify-evenly p-2 items-center">
+  //         <Link to="/profile">
+  //             <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
+  //                 <div className="text-2xl text-white hover:text-green-400">
+  //                     <BsPersonFill />
+  //                 </div>
+  //                 <h1 className="text-sm">Profile</h1>
+  //             </div>
+  //         </Link>
+  //         <Link to="/clockin">
+  //             <div className="flex flex-col justify-center items-center  rounded-full p-2 w-18 h-18 gap-1 bg-slate-300  hover:text-green-700">
+  //                 <div className="text-2xl text-white hover:text-green-400">
+  //                     <BsFillAlarmFill />
+  //                 </div>
+  //                 <h1 className="text-sm">Clock In</h1>
+  //             </div>
+  //         </Link>
+  //         <Link to="/profile/people">
+  //             <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
+  //                 <div className="text-2xl text-white hover:text-green-400">
+  //                     <BsFillPeopleFill />
+  //                 </div>
+  //                 <h1 className="text-sm">People</h1>
+  //             </div>
+  //         </Link>
+  //         <Link to="/profile/leave">
+  //             <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
+  //                 <div className="text-2xl text-white hover:text-green-400">
+  //                     <BsCalendarEvent />
+  //                 </div>
+  //                 <h1 className="text-sm">Leave</h1>
+  //             </div>
+  //         </Link>
+  //         <Link to="/profile/OT">
+  //             <div className="flex flex-col justify-center items-center  rounded-full p-2 w-16 h-16 gap-1 bg-slate-300  hover:text-green-700">
+  //                 <div className="text-2xl text-white hover:text-green-400">
+  //                     <BsClockFill />
+  //                 </div>
+  //                 <h1 className="text-sm">OT</h1>
+  //             </div>
+  //         </Link>
+  //     </div>
+  // );
 }
