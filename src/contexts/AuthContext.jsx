59b1ff487bceaf1ec8f6c9851a2cce8e3d7475c8
Prapawin <maiprapawin@gmail.com>
2023-11-01 16:42:46 +0700

    updated: AuthContext file, getMe+login & Route file (open comment)

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,32 +1,49 @@
 import { useState } from "react";
 import { createContext } from "react";
+import axios from "../config/axios";
 import {
   addAccessToken,
   getAccessToken,
   removeAccessToken,
 } from "../utils/local-storage";
+import { useEffect } from "react";
 
 export const AuthContext = createContext();
 
 export default function AuthContextProvider({ children }) {
   const [authUser, setAuthUser] = useState(null);
-  // const [initialLoading, setInitialLoading] = useState(true)
+  const [initialLoading, setInitialLoading] = useState(true);
 
-  const login = (credential) => {
-    // const res = await axios.post("/auth/login", credential);
-    // addAccessToken(res.data.accessToken); //ได้ access token ได้มาจาก res
-    // setAuthUser(res.data.user);
+  useEffect(() => {
+    if (getAccessToken()) {
+      axios
+        .get("/user/me")
+        .then((res) => {
+          setAuthUser(res.data.user);
+        })
+        .finally(() => {
+          setInitialLoading(false);
+        });
+    } else {
+      setInitialLoading(false);
+    }
+  }, []);
+
+  const login = async (credential) => {
+    const res = await axios.post("/user/login", credential);
+    addAccessToken(res.data.accessToken);
+    setAuthUser(res.data.user);
     console.log(credential);
   };
 
   const logout = () => {
-    // removeAccessToken();
-    // setAuthUser(null);
+    removeAccessToken();
+    setAuthUser(null);
     console.log("logout");
   };
 
   return (
-    <AuthContext.Provider value={{ login, logout }}>
+    <AuthContext.Provider value={{ login, logout, initialLoading, authUser }}>
       {children}
     </AuthContext.Provider>
   );
