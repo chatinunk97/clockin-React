06b473dfc13a607c14b66e64c15aa3e28cdeb25e
Prapawin <maiprapawin@gmail.com>
2023-11-01 15:40:39 +0700

    added: feature logout

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { createContext } from "react";
+import {
+  addAccessToken,
+  getAccessToken,
+  removeAccessToken,
+} from "../utils/local-storage";
 
 export const AuthContext = createContext();
 
@@ -8,10 +13,21 @@ export default function AuthContextProvider({ children }) {
   // const [initialLoading, setInitialLoading] = useState(true)
 
   const login = (credential) => {
+    // const res = await axios.post("/auth/login", credential);
+    // addAccessToken(res.data.accessToken); //ได้ access token ได้มาจาก res
+    // setAuthUser(res.data.user);
     console.log(credential);
   };
 
+  const logout = () => {
+    // removeAccessToken();
+    // setAuthUser(null);
+    console.log("logout");
+  };
+
   return (
-    <AuthContext.Provider value={{ login }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ login, logout }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
