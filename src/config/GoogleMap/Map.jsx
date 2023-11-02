b77efb4ceb29d814b5034015a73ec4f68d3ece20
chatinun <chatinun.hobby@gmail.com>
2023-11-02 09:14:25 +0700

    GoogleMap : Radius edit : done

--- src/config/GoogleMap/Map.jsx
+++ src/config/GoogleMap/Map.jsx
@@ -12,22 +12,21 @@ export default function Home({ location }) {
     googleMapsApiKey: GoogleAPI_KEY,
   });
   if (!isLoaded) return <div>Loading...</div>;
-  console.log(location);
   return <Map location={location} />;
 }
 
 function Map({ location }) {
   return (
     <GoogleMap
-      options={{ mapTypeControl: false  ,  zoomControl : false ,streetViewControl : false}}
-      zoom={19}
+      options={{ mapTypeControl: false  ,  zoomControl : true ,streetViewControl : false}}
+      zoom={17.8}
       center={location}
       mapContainerStyle={{ width: "100%", height: "100%" }}
     >
       <Marker position={location}></Marker>
       <Circle
         center={location}
-        radius={20}
+        radius={50}
         options={{
           fillColor: "red", // Change the fill color of the circle
           strokeOpacity: 0, // Change the outline opacity (0 to 1)
