84be1d5ec176b73724e88d0abae39809da8fd94b
Prapawin <maiprapawin@gmail.com>
2023-11-04 12:32:14 +0700

    updated: PeoplePage, getAllUser from the same company wiht authUser

--- src/pages/Profile/Profile_People/PeopleList.jsx
+++ src/pages/Profile/Profile_People/PeopleList.jsx
@@ -1,17 +1,28 @@
-export default function PeopleList({ image, firstName, lastName, position }) {
+import PeopleItem from "./PeopleItem";
+
+export default function PeopleList({ allPeople }) {
   return (
-    <div className="flex  bg-inputGray hover:bg-primaryGreen border rounded-[16px] mt-6 mx-6 pl-6 py-3 items-center">
-      <img
-        src={`${image}`}
-        alt=""
-        className="h-[60px] w-[60px] border rounded-full "
-      />
-      <div className="pl-10">
-        <h2 className="font-bold">
-          {firstName} {lastName}
-        </h2>
-        <h3>{position}</h3>
-      </div>
+    <div>
+      {allPeople.map((el) => (
+        <PeopleItem key={el.id} peopleObj={el} />
+      ))}
     </div>
   );
 }
+// export default function PeopleList({ image, firstName, lastName, position }) {
+//   return (
+//     <div className="flex  bg-inputGray hover:bg-primaryGreen border rounded-[16px] mt-6 mx-6 pl-6 py-3 items-center">
+//       <img
+//         src={`${image}`}
+//         alt=""
+//         className="h-[60px] w-[60px] border rounded-full "
+//       />
+//       <div className="pl-10">
+//         <h2 className="font-bold">
+//           {firstName} {lastName}
+//         </h2>
+//         <h3>{position}</h3>
+//       </div>
+//     </div>
+//   );
+// }
