7e8358bb49a36b2d840615f323f35f43a3aa7f8e
chatinun <chatinun.hobby@gmail.com>
2023-11-04 15:45:20 +0700

    Add explanation for dropdown

--- src/components/DropdownSearch.jsx
+++ src/components/DropdownSearch.jsx
@@ -3,6 +3,10 @@ import Select from 'react-select';
 
 export default function DropdownSearch({data,onChange,name}) {
 
+  /* 
+  data pattern is  an array with object structure like this
+  {label : <choiceLabel> , value : <theActualValue>}
+  */
 
   return (
     <div>
