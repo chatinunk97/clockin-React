70b5a7de00670b5feafeecf8aa289b5e13407746
Prapawin <maiprapawin@gmail.com>
2023-11-06 13:45:45 +0700

    updatd: leaveSetting UI

--- src/pages/ManagePages/Leave/ManageLeaveSetting.jsx
+++ src/pages/ManagePages/Leave/ManageLeaveSetting.jsx
@@ -1,3 +1,18 @@
+import DefaultLeaveList from "./DefaultLeaveList";
+import SubmitButton from "../../../components/SubmitButton";
+
 export default function ManageLeaveSetting() {
-  return <div>ManageLeaveSetting</div>;
+  return (
+    <div className="w-full flex flex-col m-auto items-center gap-3 p-10 mx-72 bg-gray-200 rounded-lg">
+      <h1 className="text-lg font-bold">Default Leaves</h1>
+      <div className="flex flex-col gap-3">
+        <DefaultLeaveList />
+        <DefaultLeaveList />
+        <DefaultLeaveList />
+      </div>
+      <div>
+        <SubmitButton className="rounded-xl w-20">Add</SubmitButton>
+      </div>
+    </div>
+  );
 }
