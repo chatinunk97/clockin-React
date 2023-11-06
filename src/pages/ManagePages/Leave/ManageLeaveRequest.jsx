3132a92b6d6f7f242dd088b26b92403397cfb0cf
Prapawin <maiprapawin@gmail.com>
2023-11-06 15:57:56 +0700

    added: TableLeaveRequest and map info

--- src/pages/ManagePages/Leave/ManageLeaveRequest.jsx
+++ src/pages/ManagePages/Leave/ManageLeaveRequest.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import TableLeaveRequest from "./TableLeaveRequest";
+import { dashboardAxios } from "../../../config/axios";
 
 export default function ManageLeaveRequest() {
-  return <div>ManageLeaveRequest</div>;
+  const [loading, setLoading] = useState(false);
+  const [requestLeaves, setRequestLeaves] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    dashboardAxios
+      .get("/leave/getAllRequestLeaves")
+      .then((res) => {
+        console.log(res.data.requestLeaves);
+        const leaveData = res.data.requestLeaves.map((leave) => ({
+          startDate: leave.startDate,
+          endDate: leave.endDate,
+          halfDate: leave.halfDate,
+          statusRequest: leave.statusRequest,
+          messageLeave: leave.messageLeave,
+        }));
+        console.log(leaveData);
+        setRequestLeaves(leaveData);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className=" flex flex-col justify-start md:mt-20 w-full p-2 min-w-[414px] min-h-[896px]">
+      <TableLeaveRequest requestLeaves={requestLeaves} loading={loading} />
+    </div>
+  );
 }
---
+++ src/pages/ManagePages/Leave/TableLeaveRequest.jsx
@@ -0,0 +1,39 @@
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { useState, useMemo } from "react";
+import LinearIndeterminate from "../../../components/LoadingBar";
+
+export default function TableLeaveRequest({ requestLeaves, loading }) {
+  console.log(requestLeaves);
+  const [columnDefs] = useState([
+    { field: "startDate", flex: 1 },
+    { field: "endDate", flex: 1 },
+    { field: "halfDate", flex: 1 },
+    { field: "statusRequest", flex: 1 },
+    { field: "messageLeave", flex: 1 },
+  ]);
+
+  const gridOptions = {
+    defaultColDef: {
+      resizable: true,
+      sortable: true,
+    },
+  };
+
+  const sortingOrder = useMemo(() => {
+    return ["desc", "asc", null];
+  }, []);
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 700, width: "auto" }}>
+      {loading && <LinearIndeterminate />}
+      <AgGridReact
+        rowData={requestLeaves}
+        gridOptions={gridOptions}
+        columnDefs={columnDefs}
+        sortingOrder={sortingOrder}
+      ></AgGridReact>
+    </div>
+  );
+}
