4fb5251b002f2dcd87b6ae71eb4e6bfb1ed3e192
sakaorat36 <sakaoratploy@gmail.com>
2023-11-07 18:43:19 +0700

    dashboard

--- src/pages/ManagePages/Dashboard/DashboardMainPage.jsx
+++ src/pages/ManagePages/Dashboard/DashboardMainPage.jsx
@@ -1,3 +1,49 @@
 export default function DashboardMainPage() {
-  return <div>DashboardMainPage</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <div className="flex justify-center items-center text-4xl font-semibold ">
+            Welcome to your dashboard, ACB Company
+          </div>
+          <div>Filter by Date</div>
+        </div>
+        <div>
+          <div>Total Employees</div>
+          <div>856</div>
+          <div>Employees</div>
+        </div>
+        <div>
+          <div>Late</div>
+          <div>3,342</div>
+          <div>Viewers</div>
+        </div>
+        <div>
+          <div>Leave</div>
+          <div>77</div>
+          <div>Applocants</div>
+        </div>
+        <div>
+          <div>OT</div>
+          <div>17</div>
+          <div>Employees</div>
+        </div>
+      </div>
+
+      <br />
+
+      <div>
+        <div>
+          <div>Statistics</div>
+          <div>
+            <div>On Leave</div>
+            <div>OnTime</div>
+            <div>Late</div>
+          </div>
+          <div>graph 75%</div>
+        </div>
+        <div>Employee Composition</div>
+      </div>
+    </div>
+  );
 }
