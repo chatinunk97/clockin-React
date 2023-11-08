a5183047e396174e320125798878429011f2fb20
Joamthai <joamthai@gmail.com>
2023-11-08 14:58:51 +0700

    dashBoard

--- src/pages/ManagePages/Dashboard/DashboardMainPage.jsx
+++ src/pages/ManagePages/Dashboard/DashboardMainPage.jsx
@@ -1,3 +1,19 @@
+import BarChart from "./BarChart";
+import { useState } from "react";
+
 export default function DashboardMainPage() {
-  return <div>DashboardMainPage</div>;
+  const [userData, setUserData] = useState({
+    labels: userData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: userData.map((data) => data.userGain),
+      },
+    ],
+  });
+  return (
+    <div>
+      <BarChart charData={userData} />
+    </div>
+  );
 }
