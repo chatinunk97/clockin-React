3fec36aa3d6bc6c578c97524c2ba0c00852dcd91
Prapawin <maiprapawin@gmail.com>
2023-11-08 14:42:28 +0700

    updatd success and error popup

--- src/contexts/ManageContext.jsx
+++ src/contexts/ManageContext.jsx
@@ -132,22 +132,43 @@ export default function ManageContextProvider({ children }) {
     await dashboardAxios.get("/leave/getAllLeaveProfile");
 
   const updateLeaveProfile = async (updatedLeaveProfile) => {
-    const res = await dashboardAxios.patch(
-      "leave/updateLeaveProfile",
-      updatedLeaveProfile
-    );
-    setLeaveProfileById({
-      ...leaveProfileById,
-      ...res.data.updateLeaveProfile,
-    });
-
-    getAllLeaveProfile()
-      .then((res) => {
-        setLeaveProfiles(res.data.allLeaveProfile);
-      })
-      .catch((err) => {
-        console.log(err);
+    try {
+      const res = await dashboardAxios.patch(
+        "leave/updateLeaveProfile",
+        updatedLeaveProfile
+      );
+      setLeaveProfileById({
+        ...leaveProfileById,
+        ...res.data.updateLeaveProfile,
       });
+
+      if (res.status === 200) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Edit Leave Profile success!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+
+      getAllLeaveProfile()
+        .then((res) => {
+          setLeaveProfiles(res.data.allLeaveProfile);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Something Went Wrong",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      console.error("Error:", error);
+    }
   };
 
   const getalluser = async () => {
