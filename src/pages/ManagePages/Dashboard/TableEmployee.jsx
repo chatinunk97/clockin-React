212c817f1a30d3bd4c205da8349b7ad6f387e3f1
Prapawin <maiprapawin@gmail.com>
2023-11-09 15:12:08 +0700

    refactored and created smallButton component to be used on manage pages

--- src/pages/ManagePages/Dashboard/TableEmployee.jsx
+++ src/pages/ManagePages/Dashboard/TableEmployee.jsx
@@ -6,73 +6,79 @@ import Modal from "../../../components/Modal";
 import EditemployeeForm from "../Edit/EditEmployeeFrom";
 import { useMemo } from "react";
 import LinearIndeterminate from "../../../components/LoadingBar";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import SmallButton from "../../../components/SmallButton";
 
 export default function TableEmployee({ allUser, loading }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const [UserbyId, setUserById] = useState({});
-    const [columnDefs] = useState([
-        { field: "firstName", flex: 1 },
-        { field: "lastName", flex: 1 },
-        { field: "position", flex: 1 },
-        { field: "userBossId", flex: 1 },
-        { field: "employeeId", flex: 1 },
-        { field: "mobile", flex: 1 },
-        { field: "email", flex: 1 },
-        { field: "isActive", flex: 1 },
-        {
-            field: "actionButtons",
-            headerName: "",
-            cellRenderer: (params) => (
-                <div className="flex gap-2 justify-center items-center h-full">
-                    <div className="p-2">
-                        <button
-                            onClick={() => {
-                                setUserById(params.data);
-                                setIsOpen(true);
-                            }}
-                            className="font-bold text-white w-14 h-6 bg-green-600 rounded-xl flex justify-center items-center p-2 text-center transition-transform hover:scale-105 hover:bg-green-400"
-                        >
-                            Edit
-                        </button>
-                    </div>
+  const [isOpen, setIsOpen] = useState(false);
+  const [UserbyId, setUserById] = useState({});
+  const [columnDefs] = useState([
+    { field: "firstName", flex: 1 },
+    { field: "lastName", flex: 1 },
+    { field: "position", flex: 1 },
+    { field: "userBossId", flex: 1 },
+    { field: "employeeId", flex: 1 },
+    { field: "mobile", flex: 1 },
+    { field: "email", flex: 1 },
+    { field: "isActive", flex: 1 },
+    {
+      field: "actionButtons",
+      headerName: "",
+      cellRenderer: (params) => (
+        <div className="flex gap-2 justify-center items-center h-full">
+          <div className="p-2">
+            <SmallButton
+              onClick={() => {
+                setUserById(params.data);
+                setIsOpen(true);
+              }}
+            />
+          </div>
 
-                    <div>
-                        <Link to={`/manage/employee/${params.data.id}`}>
-                            <button
-                                onClick={() => {
-                                    setUserById(params.data);
-                                }}
-                                className="font-bold text-white w-14 h-6 bg-blue-600 rounded-xl flex justify-center items-center p-2 text-center transition-transform hover:scale-105 hover:bg-blue-400"
-                            >
-                                View
-                            </button>
-                        </Link>
-                    </div>
-
-                </div >
-            ),
-        },
-    ]);
-
-    const gridOptions = {
-        defaultColDef: {
-            resizable: true,
-            sortable: true,
-        },
-    };
-
-    const sortingOrder = useMemo(() => {
-        return ["desc", "asc", null];
-    }, []);
-
-    return (
-        <div className="ag-theme-alpine" style={{ height: 700, width: "auto" }}>
-            {loading && <LinearIndeterminate />}
-            <AgGridReact rowData={allUser} gridOptions={gridOptions} columnDefs={columnDefs} sortingOrder={sortingOrder}></AgGridReact>
-            <Modal title="Edit" open={isOpen} onClose={() => setIsOpen(false)}>
-                <EditemployeeForm UserbyId={UserbyId} allUser={allUser} onClose={() => setIsOpen(false)} />
-            </Modal>
+          <div>
+            <Link to={`/manage/employee/${params.data.id}`}>
+              <SmallButton
+                onClick={() => {
+                  setUserById(params.data);
+                }}
+                bg="bg-blue-600"
+                hover="hover:bg-blue-400"
+                buttonName="View"
+              />
+            </Link>
+          </div>
         </div>
-    );
+      ),
+    },
+  ]);
+
+  const gridOptions = {
+    defaultColDef: {
+      resizable: true,
+      sortable: true,
+    },
+  };
+
+  const sortingOrder = useMemo(() => {
+    return ["desc", "asc", null];
+  }, []);
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 700, width: "auto" }}>
+      {loading && <LinearIndeterminate />}
+      <AgGridReact
+        rowData={allUser}
+        gridOptions={gridOptions}
+        columnDefs={columnDefs}
+        sortingOrder={sortingOrder}
+      ></AgGridReact>
+      <Modal title="Edit" open={isOpen} onClose={() => setIsOpen(false)}>
+        <EditemployeeForm
+          UserbyId={UserbyId}
+          allUser={allUser}
+          onClose={() => setIsOpen(false)}
+        />
+      </Modal>
+    </div>
+  );
 }
--- src/pages/ManagePages/Leave/TableLeaveSetting.jsx
+++ src/pages/ManagePages/Leave/TableLeaveSetting.jsx
@@ -6,6 +6,7 @@ import { useMemo } from "react";
 import Modal from "../../../components/Modal";
 import EditLeaveSettingForm from "./EditLeaveSettingForm";
 import LinearIndeterminate from "../../../components/LoadingBar";
+import SmallButton from "../../../components/SmallButton";
 
 export default function TableLeaveSetting({
   leaveProfiles,
@@ -24,26 +25,22 @@ export default function TableLeaveSetting({
       cellRenderer: (params) => (
         <div className="flex gap-2 justify-center items-center h-full">
           <div className="p-2">
-            <button
+            <SmallButton
               onClick={() => {
                 setLeaveProfileById(params.data);
                 setIsOpen(true);
               }}
-              className="font-bold text-white w-14 h-6 bg-green-600 rounded-xl flex justify-center items-center p-2 text-center transition-transform hover:scale-105 hover:bg-green-400"
-            >
-              Edit
-            </button>
+            />
           </div>
           <div className="p-2">
-            <button
+            <SmallButton
               onClick={() => {
-                console.log(params.data.id);
                 deleteLeaveProfile(params.data.id);
               }}
-              className="font-bold text-white w-14 h-6 bg-red-600 rounded-xl flex justify-center items-center p-2 text-center transition-transform hover:scale-105 hover:bg-red-400"
-            >
-              Delete
-            </button>
+              bg="bg-red-600"
+              hover="hover:bg-red-400"
+              buttonName="Delete"
+            />
           </div>
         </div>
       ),
