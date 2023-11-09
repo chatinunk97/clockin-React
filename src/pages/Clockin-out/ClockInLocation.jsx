1e725c81bf2c5055f1e8b99c51a250bf50ad2814
chatinun <chatinun.hobby@gmail.com>
2023-11-09 14:04:22 +0700

    User Filter done

--- src/pages/Clockin-out/ClockInLocation.jsx
+++ src/pages/Clockin-out/ClockInLocation.jsx
@@ -1,12 +1,12 @@
 import { IoLocationSharp } from "react-icons/io5";
 
-export default function ClockInlocation() {
+export default function ClockInlocation({address}) {
   return (
     <div className="flex justify-between h-full items-center px-5">
       <button className="text-3xl text-green-600 hover:text-blue-500">
         <IoLocationSharp />
       </button>
-      <p>Location Name</p>
+      <p>{address}</p>
     </div>
   );
 }
