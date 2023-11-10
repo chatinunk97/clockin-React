777102d4b6eee594892f17e3627d130322c9b0af
chatinun <chatinun.hobby@gmail.com>
2023-11-10 13:13:27 +0700

    merge issues

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -28,11 +28,8 @@ import ViewEmployee from "../pages/ManagePages/Employee.jsx/ViewEmployee";
 import ClockContextProvider from "../contexts/ClockContext";
 import LeaveContextProvider from "../contexts/LeaveContext";
 import UserContextProvider from "../contexts/UserContext";
-<<<<<<< HEAD
 import ViewLeaveRequest from "../pages/ManagePages/Leave/ViewLeaveRequest";
-=======
 import OTContextProvider from "../contexts/OTContext";
->>>>>>> develop
 
 export default function Route() {
   const router = createBrowserRouter([
