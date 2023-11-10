3be6464ca8b1858cc18b142dbe64bc50b6f78136
chatinun <chatinun.hobby@gmail.com>
2023-11-10 10:06:21 +0700

    OT context

--- src/pages/Profile/Profile_Request/OT/OTForm.jsx
+++ src/pages/Profile/Profile_Request/OT/OTForm.jsx
@@ -1,5 +1,6 @@
 import { AiFillCalendar } from "react-icons/ai";
 import { LuClock7 } from "react-icons/lu";
+import useOT from "../../../../hooks/use-OT";
 import { BsFillClipboard2Fill } from "react-icons/bs";
 import DatePicker from "react-datepicker";
 import { useState } from "react";
@@ -7,35 +8,37 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export default function OTform() {
   const [startDate, setStartDate] = useState(new Date());
-
+  const [filterClock , setFilterClock] = useState("")
+  console.log(useOT().clockList)
   return (
     <div className="flex flex-col justify-center items-start p-4 mt-12 gap-4 md:items-center md:p-20 md:mt-0">
-      <h1 className=" text-3xl font-bold">Information</h1>
+      <h1 className=" text-3xl font-bold">Informationss</h1>
       <div>
         <div className="flex items-center gap-6 w-[300px]">
-          <button className="text-3xl text-slate-700"><AiFillCalendar /></button>
+          <button className="text-3xl text-slate-700">
+            <AiFillCalendar />
+          </button>
           <DatePicker
             selected={startDate}
             onChange={(date) => setStartDate(date)}
             isClearable
             placeholderText="Select Date"
-            className='w-56 border border-stone-200 shadow-sm rounded-md cursor-pointer p-1'
-
+            className="w-56 border border-stone-200 shadow-sm rounded-md cursor-pointer p-1"
           />
         </div>
-        <div className="flex gap-4 pt-6 pb-6 text-xl justify-between">
-          <div className="flex gap-4 items-center">
-            <button className="className= text-slate-600 text-2xl"><LuClock7 /></button>
-            <div className="border-b border-b-neutral-400  text-center">17:00</div>
-          </div>
-          <div className="flex gap-4 items-center">
-            <button className="className= text-slate-600 text-2xl"><LuClock7 /></button>
-            <div className="border-b border-b-neutral-400  text-center">20:00</div>
-          </div>
+        <div className="flex">
+          <div>Clock Icon</div>
+          <div>Clock option</div>
         </div>
         <div className="flex items-center gap-4 pt-6">
-          <div className="text-3xl text-slate-700"><BsFillClipboard2Fill /></div>
-          <input type="text" placeholder="Request OT" className="border-b border-b-neutral-400  text-start p-4 focus:ring focus:ring-green-300  focus:border-green-500 outline-none" />
+          <div className="text-3xl text-slate-700">
+            <BsFillClipboard2Fill />
+          </div>
+          <input
+            type="text"
+            placeholder="Request OT"
+            className="border-b border-b-neutral-400  text-start p-4 focus:ring focus:ring-green-300  focus:border-green-500 outline-none"
+          />
         </div>
       </div>
       <hr className="w-full border-stone-300" />
@@ -57,5 +60,5 @@ export default function OTform() {
         <button className="text-xl text-white">Submit</button>
       </div>
     </div>
-  )
+  );
 }
