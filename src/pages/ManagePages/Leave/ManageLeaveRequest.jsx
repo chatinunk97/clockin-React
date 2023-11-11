d7ec954abff108e6e20f95328d08c6bf49b451f8
Prapawin <maiprapawin@gmail.com>
2023-11-11 17:52:05 +0700

    fixed bugs for viewLeaveRequestInfo button logic and removed search for leaveRequest

--- src/pages/ManagePages/Leave/ManageLeaveRequest.jsx
+++ src/pages/ManagePages/Leave/ManageLeaveRequest.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import TableLeaveRequest from "./TableLeaveRequest";
-import SearchLeave from "../../../components/SearchLeave";
 import useLeave from "../../../hooks/use-leave";
 
 export default function ManageLeaveRequest() {
@@ -18,9 +17,6 @@ export default function ManageLeaveRequest() {
 
   return (
     <div className=" flex flex-col justify-start md:mt-20 md:w-full p-2 min-w-[414px] max-h-[896px]">
-      <div className="flex justify-center items-center gap-2 w-120">
-        <SearchLeave />
-      </div>
       <div className=" flex flex-col justify-start md:mt-20 w-full p-2 min-w-[414px] min-h-[896px]">
         <TableLeaveRequest />
       </div>
