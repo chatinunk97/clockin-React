c675a421fabee548683dc09fd130bcee130b7db8
TeepakornSri <teepakorn.sriaree@gmail.com>
2023-11-11 16:48:13 +0700

    myLeaveupdate

--- src/components/MyRequestCard.jsx
+++ src/components/MyRequestCard.jsx
@@ -3,7 +3,7 @@ import { FcOvertime } from "react-icons/fc";
 
 export default function MyRequestCard({ myrequestLeave }) {
     return (
-        <div className="w-full h-36 border border-black rounded-2xl flex shadow-lg">
+        <div className="w-full h-36 border border-stone-200 rounded-2xl flex shadow-lg">
             <div className="flex h-full items-center p-2 justify-start gap-6 w-full">
                 <div className="text-5xl">
                     <FcOvertime />
@@ -11,14 +11,14 @@ export default function MyRequestCard({ myrequestLeave }) {
                 <div>
                     <div className="flex flex-col gap-2">
                         <h1>Id: {myrequestLeave.id}</h1>
-                        <h1>StartDate: {myrequestLeave.startDate}</h1>
-                        <h1>EndDate: {myrequestLeave.endDate}</h1>
+                        <h1 className="text-green-700 font-semibold">StartDate: {myrequestLeave.startDate}</h1>
+                        <h1 className="text-green-700 font-semibold">EndDate: {myrequestLeave.endDate}</h1>
                         <h1>{myrequestLeave.leaveType}</h1>
                     </div>
                 </div>
             </div>
             <div className="p-4 flex flex-col justify-end">
-                <h1>Status: {myrequestLeave.statusRequest}</h1>
+                <h1 className="text-orange-400 font-semibold">{myrequestLeave.statusRequest}</h1>
             </div>
         </div>
     );
