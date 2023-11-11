fbe9607bcb0f5492dce4da1bd9c8f62db0c9454d
Prapawin <maiprapawin@gmail.com>
2023-11-11 12:52:22 +0700

    added: ViewLeaveRequestInfo and logic for button + removed buttons if clicked already

--- src/pages/ManagePages/Leave/ViewLeaveRequest.jsx
+++ src/pages/ManagePages/Leave/ViewLeaveRequest.jsx
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { dashboardAxios } from "../../../config/axios";
 import LoadingBar from "../../../components/LoadingBar";
-import SmallButton from "../../../components/SmallButton";
-import useLeave from "../../../hooks/use-leave";
+import ViewLeaveRequestInfo from "./ViewLeaveRequestInfo";
 
 export default function ViewLeaveRequest() {
-  const { updateRequestLeave } = useLeave();
   const { requestLeaveId } = useParams();
   const [request, setRequest] = useState({});
   const [isLoading, setIsLoading] = useState(true);
@@ -26,18 +24,6 @@ export default function ViewLeaveRequest() {
       });
   }, [requestLeaveId]);
 
-  const handleApproveClick = async (e) => {
-    try {
-      e.preventDefault();
-      const input = { ...request };
-      delete input.userLeave;
-      input.statusRequest = "ACCEPT";
-      console.log(input);
-      await updateRequestLeave(input);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <>
       {isLoading ? (
@@ -50,37 +36,11 @@ export default function ViewLeaveRequest() {
               {request.userLeave.user.lastName}
             </h1>
             <br />
-            <div className="text-lg flex gap-60">
-              <div>
-                <div>
-                  Leave Name: {request.userLeave.leaveProfile.leaveName}
-                </div>
-                <div>Start Date: {request.startDate}</div>
-                <div>End Date: {request.endDate}</div>
-                <div>Leave Type: {request.leaveType}</div>
-                <div>Leave Message: {request.messageLeave}</div>
-              </div>
-              <div>
-                <div>
-                  Leave Quota:{" "}
-                  {request.userLeave && request.userLeave.dateAmount} days
-                </div>
-                <div>Leave Quota Left: XXXX days</div>
-                <div>Request Status: {request.statusRequest}</div>
-                <br />
-                <div className="flex gap-3">
-                  <SmallButton
-                    onClick={handleApproveClick}
-                    buttonName="Approve"
-                  />
-                  <SmallButton
-                    buttonName="Reject"
-                    bg="bg-red-600"
-                    hover="hover:bg-red-400"
-                  />
-                </div>
-              </div>
-            </div>
+            <ViewLeaveRequestInfo
+              request={request}
+              setRequest={setRequest}
+              requestLeaveId={requestLeaveId}
+            />
           </div>
         </>
       )}
