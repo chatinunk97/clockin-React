497477df43984f770db2281d04b7eea83a1a3317
Tar <Tarta12296@gmail.com>
2023-11-13 11:43:49 +0700

    wait pull

--- src/components/DashboardPieChart.jsx
+++ src/components/DashboardPieChart.jsx
@@ -1,27 +1,48 @@
-import { PieChart } from "@mui/x-charts/PieChart";
+import React, { useEffect } from "react";
+import Chart from "chart.js/auto";
 
 export default function DashboardPieChart({ chartData }) {
+  useEffect(() => {
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: chartData.labels,
+        datasets: [
+          {
+            data: chartData.data,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.7)",
+              "rgba(54, 162, 235, 0.7)",
+              "rgba(255, 206, 86, 0.7)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          position: "bottom",
+          labels: {
+            fontColor: "black",
+            fontSize: 14,
+            padding: 20,
+          },
+        },
+      },
+    });
+  }, [chartData]);
+
   return (
     <div>
-      <PieChart
-        series={[
-          {
-            // data: chartData, // Use the retrieved data
-            data: chartData,
-            highlightScope: {
-              faded: "global",
-              highlighted: "item",
-            },
-            faded: {
-              innerRadius: 30,
-              additionalRadius: -30,
-              color: "gray",
-            },
-          },
-        ]}
-        height={200}
-        width={405}
-      />
+      <canvas id="myChart"></canvas>
     </div>
   );
 }
--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -9,12 +9,9 @@ export default function MainLayout() {
   const { logout } = useAuth();
   const page = useLocation().pathname;
   console.log(page);
-  console.log(page);
   return (
     <div className="flex flex-col h-screen min-h-[667px] min-w-[375px] max-w-[1024px] m-auto">
       <Header onClick={logout}></Header>
-    <div className="flex flex-col max-w-4xl m-auto  h-screen min-h-[667px] min-w-[375px]">
-      <Header onClick={logout}>{page}</Header>
       <div className="overflow-auto h-full">
         {page === "/" ? <Navigate to={"/clockin"} /> : <Outlet />}
       </div>
