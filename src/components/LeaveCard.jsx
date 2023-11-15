47239cb336e22f9a05465afa24803e2cda5d3760
TeepakornSri <teepakorn.sriaree@gmail.com>
2023-11-15 11:48:57 +0700

    ProfileviewCalendar

--- src/components/LeaveCard.jsx
+++ src/components/LeaveCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function LeaveCard({ startDate, endDate, messageLeave }) {
   return (
-    <div className="p-3 shadow-md">
-      LeaveCard
-      <p>{startDate}</p>
-      <p>{endDate}</p>
-      <p>{messageLeave}</p>
+    <div className="p-3 shadow-lg w-80 md:w-full mt-8 rounded-lg ">
+      <h1 className="text-lg font-bold text-green-600">LeaveDetail</h1>
+      <p >Start Date : {startDate}</p>
+      <p>End Date : {endDate}</p>
+      <p>Leave Information : {messageLeave}</p>
     </div>
   );
 }
